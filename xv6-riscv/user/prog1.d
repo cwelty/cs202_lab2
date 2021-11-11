1d90de8e0ab76d827be54744fca3b23fd0aed86f
Miguel <mroja013@ucr.edu>
2021-11-11 01:48:17 -0800

    Lottery good; tweaking stride

--- xv6-riscv/user/prog1.d
+++
@@ -1,3 +0,0 @@
-user/prog1.o: user/prog1.c kernel/types.h kernel/stat.h user/user.h \
- kernel/rng.c /usr/lib/gcc-cross/riscv64-linux-gnu/9/include/stdint.h \
- /usr/lib/gcc-cross/riscv64-linux-gnu/9/include/stdint-gcc.h
